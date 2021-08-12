48808c12e33a2746b59c13f0f9ebd2ebcc608bff
Michal <mwietesk@ire.pw.edu.pl>
2021-08-12 18:10:51 +0200

    Zapis do bazy oraz odczyt z bazy dzialaja.

--- src/main/java/com/example/ladowarka1/FirstFragment.java
+++ src/main/java/com/example/ladowarka1/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.example.ladowarka1;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,9 @@ import com.example.ladowarka1.databinding.FragmentFirstBinding;
 
 //import sun.net.www.http.HttpClient;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.HttpURLConnection;
@@ -26,11 +30,19 @@ public class FirstFragment extends Fragment {
 
     int onOffButtonState = 0;
 
+    int counter = 0;
+
+    String POSTResponse = "";
+
+    Handler handlerSendPOSTChangeState = new Handler();
+    Handler handlerSendPOSTReadState = new Handler();
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-    ) {
+    )
+    {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -40,41 +52,27 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-
         binding.onoffbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (onOffButtonState==0)
-                {
+                if (onOffButtonState == 0) {
                     binding.onoffbutton.setText("OFF");
                     onOffButtonState = 1;
 
-                    try
-                    {
-                        SendPOST(1);
-                    } catch (IOException e)
-                    {
+                    try {
+                        SendPOSTChangeState(1, view);
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-                else
-                {
+                else {
                     binding.onoffbutton.setText("ON");
                     onOffButtonState = 0;
 
-                    try
-                    {
-                        SendPOST(0);
-                    } catch (IOException e)
-                    {
+                    try {
+                        SendPOSTChangeState(0, view);
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
@@ -82,7 +80,18 @@ public class FirstFragment extends Fragment {
                 view.refreshDrawableState();
             }
         });
+
+
+        try {
+            CyclicOperations(view);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+
 
     @Override
     public void onDestroyView() {
@@ -90,9 +99,9 @@ public class FirstFragment extends Fragment {
         binding = null;
     }
 
-    public void SendPOST(int state) throws IOException {
+    public void SendPOSTChangeState(int state, View view) throws IOException {
 
-        final String[] logText = {"aaa0"};
+        final String[] logText = {"default"};
 
         Thread thread = new Thread(new Runnable() {
 
@@ -104,8 +113,6 @@ public class FirstFragment extends Fragment {
                     String data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Request>\n  <CLEAR>@@@</CLEAR>\n  <ID_LADOWARKI>@@@</ID_LADOWARKI>\n  <STATUS_LADOWARKI_READ>@@@</STATUS_LADOWARKI_READ>\n  <STATUS_LADOWARKI_WRITE>@@@</STATUS_LADOWARKI_WRITE>\n </Request>";
                     int intVal;
 
-                    logText[0] = "aaa1";
-
                     intVal = 0;
                     data=data.replaceFirst("@@@",Integer.toString(intVal));
                     intVal = 1;
@@ -115,20 +122,13 @@ public class FirstFragment extends Fragment {
                     intVal = state;
                     data=data.replaceFirst("@@@",Integer.toString(intVal));
 
-                    logText[0] = "aaa2";
-
                     URL url = new URL("http://www.greenyy.webd.pro/interfaces/ladowarka.php");
                     HttpURLConnection http = (HttpURLConnection)url.openConnection();
                     http.setRequestMethod("POST");
                     http.setDoOutput(true);
                     http.setRequestProperty("Content-Type", "application/xml");
 
-                    logText[0] = "aaa3";
-                    //binding.textviewFirst.setText(data);
-
                     byte[] out = data.getBytes(StandardCharsets.UTF_8);
-
-                    logText[0] = "aaa4";
 
                     try {
                         OutputStream stream = http.getOutputStream();
@@ -136,37 +136,137 @@ public class FirstFragment extends Fragment {
                     }
                     catch (Exception e)
                     {
-                        binding.textviewFirst.setText(e.toString());
-                        //binding.textviewFirst.setText(data);
                     }
 
-                    //binding.textviewFirst.setText("aaa6");
-
-                    logText[0] = http.getResponseMessage();
-
-                    //binding.textviewFirst.setText("aaa7");
-
-                    //binding.textviewFirst.setText(data);
+                    POSTResponse = http.getResponseMessage();
 
                     http.disconnect();
+
+                    handlerSendPOSTChangeState.post(new Runnable() {
+                        public void run() {
+                            //binding.textviewFirst.setText(POSTResponse);
+                            view.refreshDrawableState();
+                        }
+                    });
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-
-
             }
         });
 
         thread.start();
-
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        binding.textviewFirst.setText(logText[0]);
     }
+
+    public void SendPOSTReadState(View view) throws IOException {
+
+        String message = "Connection ERROR";
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try
+                {
+
+                    String data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Request>\n  <CLEAR>@@@</CLEAR>\n  <ID_LADOWARKI>@@@</ID_LADOWARKI>\n  <STATUS_LADOWARKI_READ>@@@</STATUS_LADOWARKI_READ>\n </Request>";
+                    int intVal;
+
+                    intVal = 0;
+                    data=data.replaceFirst("@@@",Integer.toString(intVal));
+                    intVal = 1;
+                    data=data.replaceFirst("@@@",Integer.toString(intVal));
+                    intVal = 1;
+                    data=data.replaceFirst("@@@",Integer.toString(intVal));
+
+                    URL url = new URL("http://www.greenyy.webd.pro/interfaces/ladowarka.php");
+                    HttpURLConnection http = (HttpURLConnection)url.openConnection();
+                    http.setRequestMethod("POST");
+                    http.setDoOutput(true);
+                    http.setRequestProperty("Content-Type", "application/xml");
+
+                    byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+                    try {
+                        OutputStream stream = http.getOutputStream();
+                        stream.write(out);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+                    ///*
+                    InputStream inputStream = http.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line = "";
+                    String response = "";
+                    POSTResponse = response;
+
+                    line = reader.readLine();
+
+                    while(line != null)
+                    {
+                        response = response.concat(line);
+                        line = reader.readLine();
+                    }
+
+                    POSTResponse = response;
+
+                    //response = response.concat(line);
+
+                    //*/
+                    http.disconnect();
+
+                    handlerSendPOSTReadState.post(new Runnable() {
+                        public void run() {
+                            binding.textviewFirst.setText(POSTResponse);
+                            //binding.textviewFirst.setText(data);
+                            //binding.textviewFirst.setText(Integer.toString(counter));
+                            view.refreshDrawableState();
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    public void CyclicOperations(View view) throws IOException
+    {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+
+                        SendPOSTReadState(view);
+
+                        counter++;
+                        handlerSendPOSTReadState.post(new Runnable() {
+                            public void run() {
+                                //binding.textviewFirst.setText(Integer.toString(counter));
+                                //view.refreshDrawableState();
+                            }
+                        });
+
+                        Thread.sleep(500);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+
 }
